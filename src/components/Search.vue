dea6b0fdad793abb414ec6c32f3a5f2bfd197a41
Nora.Hu <nora.h@thinkwithu.com>
2021-11-12 13:56:13 +0800

    Vu3 Composition API使用Vuex

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,20 +1,35 @@
 <template>
   <div>
-      <input type="text" :value="$store.state.search" @input="onChange">
-      <span>城市：{{$store.getters.searchText}}</span>
+    <input type="text" :value="search" @input="onChange" />
+    <span>城市：{{ searchText }}</span>
   </div>
 </template>    
 
 
 <script>
+import { computed } from "vue";
+import { useStore } from "vuex";
 export default {
-    methods:{
-        onChange(e){
-            this.$store.commit('setSearch',e.target.value)
-            console.log(e.target.value);
-            this.$store.dispatch('getWeather', e.target.value)
-        }
-    }
+  // methods:{
+  //     onChange(e){
+  //         this.$store.commit('setSearch',e.target.value)
+  //         console.log(e.target.value);
+  //         this.$store.dispatch('getWeather', e.target.value)
+  //     }
+  // }
+  setup() {
+    let store = useStore();
+    const onChange = (e) => {
+      store.commit("setSearch", e.target.value);
+      console.log(e.target.value);
+      store.dispatch("getWeather", e.target.value);
+    };
+    return {
+      search: computed(() => store.state.search),
+      searchText: computed(() => store.getters.searchText),
+      onChange,
+    };
+  },
 };
 </script>
 
